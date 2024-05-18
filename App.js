ea60220c3c5a0009ae2c67f8f58b0a2d589dafe8
Bukhbilegt Tuvshinzaya <52137140+Ultimatetsuivan@users.noreply.github.com>
2024-05-18 08:59:21 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,43 +1,40 @@
-import React, { useState, useEffect } from "react";
-
-import MovieCard from "./MovieCard";
-import SearchIcon from "./search.svg";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-const API_URL = "http://www.omdbapi.com?apikey=99b2346e";
+//99b2346e
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=99b2346e";
+
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    searchMovies("Batman");
-  }, []);
-
+  const [searchTerm, setSearchTerm] = useState('');
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
+    const response = await fetch(`${API_URL}&s={title}`);
     const data = await response.json();
-
     setMovies(data.Search);
   };
+
+  useEffect(() => {
+    searchMovies("Superman");
+  }, []);
 
   return (
     <div className="app">
       <h1>MovieLand</h1>
-
       <div className="search">
         <input
+          placeholder="Search for movies"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
         />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => searchMovies(searchTerm)}
-        />
+        <img src={SearchIcon} 
+        alt="search" 
+        onClick={() =>searchMovies(searchTerm)}></img>
       </div>
-
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
@@ -45,9 +42,7 @@ const App = () => {
           ))}
         </div>
       ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
+        <div className="empty">No movies found</div>
       )}
     </div>
   );
